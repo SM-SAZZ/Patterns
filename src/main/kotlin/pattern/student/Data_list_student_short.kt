7cc942b4d9cb3decf2a826bc392c35183ab7eaa0
SAZZ <malakeev03@mail.ru>
2024-11-26 03:34:10 +0300

    Л2/З2.9. Добавил поле контактов к выводу и диаграмму классов

--- src/main/kotlin/pattern/student/Data_list_student_short.kt
+++ src/main/kotlin/pattern/student/Data_list_student_short.kt
@@ -6,10 +6,10 @@ import org.sazz.student.Student_short
 
 class Data_list_student_short(students: List<Student_short>) : Data_list<Student_short>(students) {
     override fun getEntityFields(): List<String> {
-        return listOf("Имя", "Гит")
+        return listOf("Имя", "Гит", "Контакт")
     }
 
     override fun getDataRow(entity: Student_short): List<Any> {
-        return listOf(entity.lastNameInitials, entity.git) as List<Any>
+        return listOf(entity.lastNameInitials, entity.git, entity.contact) as List<Any>
     }
 }
