f55fc7fc35359882016f16ae4ec4c02ec46eacf7
SAZZ <malakeev03@mail.ru>
2024-11-26 03:30:33 +0300

    Л2/З2.7-8. Использован паттерн "Шаблон" и протестирован на примере Student_short

--- src/main/kotlin/pattern/student/Data_list_student_short.kt
+++ src/main/kotlin/pattern/student/Data_list_student_short.kt
@@ -5,13 +5,11 @@ import org.sazz.pattern.Data_table
 import org.sazz.student.Student_short
 
 class Data_list_student_short(students: List<Student_short>) : Data_list<Student_short>(students) {
-    override fun getNames(): List<String> {
-        return listOf("№", "Имя", "Гит")
+    override fun getEntityFields(): List<String> {
+        return listOf("Имя", "Гит")
     }
-    override fun getData(): Data_table<Any> {
-        val data = elements.mapIndexed { index, student ->
-            listOf(index + 1, student.lastNameInitials, student.git)
-        }
-        return Data_table.create(data) as Data_table<Any>
+
+    override fun getDataRow(entity: Student_short): List<Any> {
+        return listOf(entity.lastNameInitials, entity.git) as List<Any>
     }
 }
