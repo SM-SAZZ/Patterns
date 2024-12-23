1df5d3a16b1d6984d8613c1d6b5ba6180ae62d20
SAZZ <malakeev03@mail.ru>
2024-12-24 00:24:05 +0300

    Л4/З1. Поднята БД PostgreSQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
     implementation("com.charleskorn.kaml:kaml:0.61.0")
     testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.postgresql:postgresql:42.2.20")
 }
 
 tasks.test {
